d7aae65e5f2c0d5ee0b4bdc9023ff05bca2538b0
Tommy-de-oz1thc <60524067+Tommy-de-oz1thc@users.noreply.github.com>
2025-07-17 22:05:04 +0200

    Add files via upload

---
+++ db/comment-qso-phrases-db.asm
@@ -0,0 +1,226 @@
+; Autogenereret NASM db-liste
+ord1 db "hw",0 
+ord2 db "rst 599",0 
+ord3 db "name peter",0 
+ord4 db "ur sig rst 599",0 
+ord5 db "ant 3 band quad",0 
+ord6 db "so btu hw nw",0 
+ord7 db "tnx fer rpt",0 
+ord8 db "tnx call",0 
+ord9 db "ur sigs 599 es gud condx hre",0 
+ord10 db "running 150 watts",0 
+ord11 db "hw cpy",0 
+ord12 db "name here is steve ",0 
+ord13 db "rst is also 599",0 
+ord14 db "been ham since 1983",0 
+ord15 db "hr name is freddy",0 
+ord16 db "pwr 100w",0 
+ord17 db "wx is raining es cool",0 
+ord18 db "nice 5nn sig",0 
+ord19 db "73 es gud dx",0 
+ord20 db "cu soon",0 
+ord21 db "nice to work you fer first time",0 
+ord22 db "into trap dipole",0 
+ord23 db "name here is mike",0 
+ord24 db "73 es cuagn",0 
+ord25 db "ant 3 band yagi",0 
+ord26 db "rig k3s",0 
+ord27 db "running 50 watts",0 
+ord28 db "hpe cpy u agn soon",0 
+ord29 db "tnx fer buzz",0 
+ord30 db "up 50ft",0 
+ord31 db "ur rst 5nn wid some qrm",0 
+ord32 db "wx hr is warm es dry",0 
+ord33 db "name is bob",0 
+ord34 db "pse qrs",0 
+ord35 db "fb",0 
+ord36 db "ant dipole",0 
+ord37 db "btu for your qth",0 
+ord38 db "pwr 1tt watts",0 
+ord39 db "been ham since 1996",0 
+ord40 db "ant g5rv",0 
+ord41 db "running 1tt watts",0 
+ord42 db "qth is tokyo",0 
+ord43 db "wx hot es humid",0 
+ord44 db "599 599",0 
+ord45 db "name is tom",0 
+ord46 db "fb om",0 
+ord47 db "r all fb om",0 
+ord48 db "rst 599 es no qrm or qsb",0 
+ord49 db "qsl direct",0 
+ord50 db "mni tks fer qso",0 
+ord51 db "name is ted",0 
+ord52 db "up 30ft",0 
+ord53 db "ant 5 band vert",0 
+ord54 db "running 25 watts",0 
+ord55 db "wx hot",0 
+ord56 db "op hr is bob",0 
+ord57 db "wx hot es cloudy",0 
+ord58 db "fb on that",0 
+ord59 db "been licensed since 2000",0 
+ord60 db "what rig r u using",0 
+ord61 db "ge om",0 
+ord62 db "hpe qso agn",0 
+ord63 db "pwr 50w",0 
+ord64 db "best 73",0 
+ord65 db "pse qsl via buro",0 
+ord66 db "fb onqso om cu agn soon",0 
+ord67 db "some qsb on last tx",0 
+ord68 db "hre ot op age 80 years",0 
+ord69 db "gm tnx call",0 
+ord70 db "qth is moscow",0 
+ord71 db "missed your name in qsb",0 
+ord72 db "name alex",0 
+ord73 db "wx cloudy es temp 15c",0 
+ord74 db "gm dr om",0 
+ord75 db "mni tks fer sig rprt good luck",0 
+ord76 db "btu for your report",0 
+ord77 db "got to go",0 
+ord78 db "3 ele beam at 100ft",0 
+ord79 db "mni tks fer nice qso",0 
+ord80 db "rst 5nn 599",0 
+ord81 db "qth hr is london",0 
+ord82 db "qth",0 
+ord83 db "hpe cul om",0 
+ord84 db "ok om mni tks fer call",0 
+ord85 db "qsl via buro",0 
+ord86 db "running 5 watts",0 
+ord87 db "rig is home brew",0 
+ord88 db "pse agn",0 
+ord89 db "ur rst 559",0 
+ord90 db "ga dr om",0 
+ord91 db "wx cold es cloudy",0 
+ord92 db "running 15t watts",0 
+ord93 db "up 2",0 
+ord94 db "hpe cu agn om",0 
+ord95 db "rig hr is ic730",0 
+ord96 db "mni tnx for short qso",0 
+ord97 db "been ham fer 20 yrs",0 
+ord98 db "sig is rst 439",0 
+ord99 db "nw qrt",0 
+ord100 db "rig kenwood ts2000x",0 
+ord101 db "must go soon",0 
+ord102 db "hi hi",0 
+ord103 db "hw cpy nw om",0 
+ord104 db "missed your name in qrm",0 
+ord105 db "name is rob",0 
+ord106 db "name luke",0 
+ord107 db "best wishes",0 
+ord108 db "pse agn rst es qth",0 
+ord109 db "tks for the call",0 
+ord110 db "running 5t watts",0 
+ord111 db "wx snowing es vy cold",0 
+ord112 db "rig kx3",0 
+ord113 db "ga tnx call",0 
+ord114 db "vy gd om es tnx fer qso",0 
+ord115 db "wx rain es cold",0 
+ord116 db "name paul",0 
+ord117 db "r r solid cpy",0 
+ord118 db "op age 65 years",0 
+ord119 db "cu agn",0 
+ord120 db "my name joe",0 
+ord121 db "tks fer nice qso",0 
+ord122 db "name tim",0 
+ord123 db "pse agn ur name",0 
+ord124 db "hpe sigs gud so btu",0 
+ord125 db "rst is 559",0 
+ord126 db "rst is 457 wid qsb",0 
+ord127 db "hr ant is long wire",0 
+ord128 db "been ham since 1975",0 
+ord129 db "qth is melbourne",0 
+ord130 db "qth new york",0 
+ord131 db "hpe cuagn",0 
+ord132 db "must go qrt soon",0 
+ord133 db "hpe u hve gd dx best 73",0 
+ord134 db "pse agn rst",0 
+ord135 db "name is ron",0 
+ord136 db "wx cool es foggy",0 
+ord137 db "tu fer qso",0 
+ord138 db "so btu",0 
+ord139 db "hr name paul",0 
+ord140 db "tnx fer qso",0 
+ord141 db "what is my rst pse",0 
+ord142 db "nice to hear from you",0 
+ord143 db "handle is eric",0 
+ord144 db "op age 73 years",0 
+ord145 db "so hw cpy",0 
+ord146 db "name is dan",0 
+ord147 db "hr name is tom",0 
+ord148 db "hr pwr is qrp 1w",0 
+ord149 db "rst 599 599",0 
+ord150 db "ant 5 band yagi",0 
+ord151 db "rst 5nn with gud sig",0 
+ord152 db "rst 5nn",0 
+ord153 db "tx wid 1 watt",0 
+ord154 db "hr rig is ts830",0 
+ord155 db "name",0 
+ord156 db "wx cold es snowing hre",0 
+ord157 db "hw my sigs now",0 
+ord158 db "pwr is 5w qrp",0 
+ord159 db "qth wellington",0 
+ord160 db "op hr is john",0 
+ord161 db "up 3",0 
+ord162 db "ur rst is 579",0 
+ord163 db "rig icom7300",0 
+ord164 db "agn",0 
+ord165 db "hpe cu agn soon",0 
+ord166 db "nice to qso agn",0 
+ord167 db "wx is warm temp 23c",0 
+ord168 db "tnx fer call",0 
+ord169 db "up 25ft",0 
+ord170 db "rig ft450d",0 
+ord171 db "73 om",0 
+ord172 db "qth is london",0 
+ord173 db "rig kx2",0 
+ord174 db "hpe cul 73",0 
+ord175 db "ur rst 599",0 
+ord176 db "vy weak pse agn rst es name",0 
+ord177 db "rig ft102 wid 10w pwr",0 
+ord178 db "hr pwr abt 10 watts",0 
+ord179 db "rst is 469 wid some qrm",0 
+ord180 db "ge tnx call",0 
+ord181 db "wx cloudy es temp 75f",0 
+ord182 db "ant is mag loop",0 
+ord183 db "100w to dipole",0 
+ord184 db "new qrp rig",0 
+ord185 db "nr",0 
+ord186 db "ant vertical",0 
+ord187 db "name is pat",0 
+ord188 db "name here is tom",0 
+ord189 db "up 1",0 
+ord190 db "rig is homebrew",0 
+ord191 db "pse qsl direct ok",0 
+ord192 db "gm om",0 
+ord193 db "fb nice to meet u",0 
+ord194 db "qth hr is rome",0 
+ord195 db "ga om",0 
+ord196 db "been ham fer 13 yrs",0 
+ord197 db "pse qsl direct",0 
+ord198 db "going qrt",0 
+ord199 db "hre ot op age 85 years",0 
+ord200 db "tnx fer nice qso",0 
+ord201 db "ge dr om",0 
+ord202 db "rig ft1000",0 
+ord203 db "pse agn qth",0 
+; Midlertidig liste af word_ptrs
+word_ptrs dd ord1,ord2,ord3,ord4,ord5,ord6,ord7,ord8,ord9,ord10 
+ dd ord11,ord12,ord13,ord14,ord15,ord16,ord17,ord18,ord19,ord20 
+ dd ord21,ord22,ord23,ord24,ord25,ord26,ord27,ord28,ord29,ord30 
+ dd ord31,ord32,ord33,ord34,ord35,ord36,ord37,ord38,ord39,ord40 
+ dd ord41,ord42,ord43,ord44,ord45,ord46,ord47,ord48,ord49,ord50 
+ dd ord51,ord52,ord53,ord54,ord55,ord56,ord57,ord58,ord59,ord60 
+ dd ord61,ord62,ord63,ord64,ord65,ord66,ord67,ord68,ord69,ord70 
+ dd ord71,ord72,ord73,ord74,ord75,ord76,ord77,ord78,ord79,ord80 
+ dd ord81,ord82,ord83,ord84,ord85,ord86,ord87,ord88,ord89,ord90 
+ dd ord91,ord92,ord93,ord94,ord95,ord96,ord97,ord98,ord99,ord100 
+ dd ord101,ord102,ord103,ord104,ord105,ord106,ord107,ord108,ord109,ord110 
+ dd ord111,ord112,ord113,ord114,ord115,ord116,ord117,ord118,ord119,ord120 
+ dd ord121,ord122,ord123,ord124,ord125,ord126,ord127,ord128,ord129,ord130 
+ dd ord131,ord132,ord133,ord134,ord135,ord136,ord137,ord138,ord139,ord140 
+ dd ord141,ord142,ord143,ord144,ord145,ord146,ord147,ord148,ord149,ord150 
+ dd ord151,ord152,ord153,ord154,ord155,ord156,ord157,ord158,ord159,ord160 
+ dd ord161,ord162,ord163,ord164,ord165,ord166,ord167,ord168,ord169,ord170 
+ dd ord171,ord172,ord173,ord174,ord175,ord176,ord177,ord178,ord179,ord180 
+ dd ord181,ord182,ord183,ord184,ord185,ord186,ord187,ord188,ord189,ord190 
+ dd ord191,ord192,ord193,ord194,ord195,ord196,ord197,ord198,ord199,ord200 
+ dd ord201,ord202,ord203
---
+++ db/dit-challenge-db.asm
@@ -0,0 +1,353 @@
+; Autogenereret NASM db-liste
+ord1 db "55 bees",0 
+ord2 db "he uses this",0 
+ord3 db "he sees her trees",0 
+ord4 db "he is sheepish",0 
+ord5 db "he sheers her sheep",0 
+ord6 db "The cheese is his",0 
+ord7 db "fish in the sea",0 
+ord8 db "hide his bees",0 
+ord9 db "he sees bees",0 
+ord10 db "her sheep sighs",0 
+ord11 db "these dishes",0 
+ord12 db "kiss the bees",0 
+ord13 db "side of his sister",0 
+ord14 db "she sees his trees",0 
+ord15 db "she is sheepish",0 
+ord16 db "kiss his sheets",0 
+ord17 db "sense of size",0 
+ord18 db "these bees are his",0 
+ord19 db "she seeks ash",0 
+ord20 db "she kisses her sister",0 
+ord21 db "she said shh",0 
+ord22 db "hide the bees",0 
+ord23 db "size of her sister",0 
+ord24 db "his 55 fish",0 
+ord25 db "is he here",0 
+ord26 db "fish in the sea",0 
+ord27 db "these sheep hide",0 
+ord28 db "sheep by seashore",0 
+ord29 db "hide the dishes",0 
+ord30 db "shes his sister",0 
+ord31 db "this is his sheep",0 
+ord32 db "hiss her sheets",0 
+ord33 db "these bees",0 
+ord34 db "55 bees es 55 sheep",0 
+ord35 db "she sees bees",0 
+ord36 db "this is cheese",0 
+ord37 db "his sheep sigh",0 
+ord38 db "she is 55",0 
+ord39 db "hide these sheep",0 
+ord40 db "the sri trees",0 
+ord41 db "here is hash",0 
+ord42 db "she sighs",0 
+ord43 db "hide the trees",0 
+ord44 db "side of his sheep",0 
+ord45 db "she sees ash",0 
+ord46 db "her sense",0 
+ord47 db "set of sheets",0 
+ord48 db "he sees her",0 
+ord49 db "bees seek them",0 
+ord50 db "he is she",0 
+ord51 db "this is fish",0 
+ord52 db "use her sheets",0 
+ord53 db "kiss the sheep",0 
+ord54 db "she sees him",0 
+ord55 db "he hisses at her",0 
+ord56 db "the sister uses this",0 
+ord57 db "his 55 fish",0 
+ord58 db "she es he",0 
+ord59 db "side of his cheese",0 
+ord60 db "side of cheese",0 
+ord61 db "she is his sister",0 
+ord62 db "her sri sister",0 
+ord63 db "he uses the dishes",0 
+ord64 db "fish in the sea",0 
+ord65 db "this is him",0 
+ord66 db "did she see this",0 
+ord67 db "seems she is",0 
+ord68 db "these three dishes",0 
+ord69 db "is she here",0 
+ord70 db "her sister sees bees",0 
+ord71 db "he has a disease",0 
+ord72 db "hide the sheets",0 
+ord73 db "he uses these trees",0 
+ord74 db "her 55 sheep",0 
+ord75 db "she uses the dishes",0 
+ord76 db "is her sister here",0 
+ord77 db "he seeks cheese",0 
+ord78 db "she is sissier",0 
+ord79 db "she wishes",0 
+ord80 db "Here is Tennessee",0 
+ord81 db "hide her sheep",0 
+ord82 db "is she he",0 
+ord83 db "these three hens",0 
+ord84 db "she sees her sheep",0 
+ord85 db "seems to hide",0 
+ord86 db "55 sheep",0 
+ord87 db "her sister sees dishes",0 
+ord88 db "fish in the sea",0 
+ord89 db "hens es bees",0 
+ord90 db "three tree sizes",0 
+ord91 db "Here is Mississippi",0 
+ord92 db "this is ash",0 
+ord93 db "side of sheep",0 
+ord94 db "she has ash",0 
+ord95 db "his sri trees",0 
+ord96 db "this is her sheep",0 
+ord97 db "The cheese is hers",0 
+ord98 db "hide his sister",0 
+ord99 db "she used this",0 
+ord100 db "she sighs at him",0 
+ord101 db "his sense of her",0 
+ord102 db "sheer the sheep",0 
+ord103 db "Mississippi fish",0 
+ord104 db "he is 55",0 
+ord105 db "she sighs",0 
+ord106 db "hide his hen",0 
+ord107 db "sheep by seashore",0 
+ord108 db "sheer sheets",0 
+ord109 db "she uses this",0 
+ord110 db "size of him",0 
+ord111 db "she has a disease",0 
+ord112 db "this sheep is hiding",0 
+ord113 db "use his dishes",0 
+ord114 db "he sighs",0 
+ord115 db "side of cheese",0 
+ord116 db "hash es fish",0 
+ord117 db "she is sri",0 
+ord118 db "she sees cheese",0 
+ord119 db "he is sri",0 
+ord120 db "sheer sheets",0 
+ord121 db "side of fish",0 
+ord122 db "sissy sissies",0 
+ord123 db "set the dishes",0 
+ord124 db "his 55 fish",0 
+ord125 db "these fishes",0 
+ord126 db "her sisters sense",0 
+ord127 db "Tennesse bees",0 
+ord128 db "bees seek his hash",0 
+ord129 db "55 sisters",0 
+ord130 db "did she seek her",0 
+ord131 db "these wishes",0 
+ord132 db "he wishes",0 
+ord133 db "her sister sighs",0 
+ord134 db "he is sissier",0 
+ord135 db "she sighs at her",0 
+ord136 db "three sri trees",0 
+ord137 db "he kisses her",0 
+ord138 db "did he seek this",0 
+ord139 db "these dishes",0 
+ord140 db "the size of her",0 
+ord141 db "did he see this",0 
+ord142 db "this is hash",0 
+ord143 db "is in these trees",0 
+ord144 db "the sri sister",0 
+ord145 db "the fish is sri",0 
+ord146 db "he is sheepish",0 
+ord147 db "she is sheepish",0 
+ord148 db "he uses this",0 
+ord149 db "hide the bees",0 
+ord150 db "the sister uses this",0 
+ord151 db "these fish at sea",0 
+ord152 db "hide the trees",0 
+ord153 db "this is hash",0 
+ord154 db "three tree sizes",0 
+ord155 db "these bees",0 
+ord156 db "size of her sister",0 
+ord157 db "The cheese is hers",0 
+ord158 db "use his dishes",0 
+ord159 db "the sri trees",0 
+ord160 db "sheep by seashore",0 
+ord161 db "she has a disease",0 
+ord162 db "kiss the bees",0 
+ord163 db "side of fish",0 
+ord164 db "she sighs at him",0 
+ord165 db "hens es bees",0 
+ord166 db "hide his bees",0 
+ord167 db "her sheep sighs",0 
+ord168 db "hide his hen",0 
+ord169 db "he is sissier",0 
+ord170 db "hide her sheep",0 
+ord171 db "she is his sister",0 
+ord172 db "did she see this",0 
+ord173 db "she sighs at her",0 
+ord174 db "this sheep is hiding",0 
+ord175 db "she uses this",0 
+ord176 db "these dishes",0 
+ord177 db "her fish",0 
+ord178 db "he has a disease",0 
+ord179 db "Tennesse bees",0 
+ord180 db "is she he",0 
+ord181 db "shes his sister",0 
+ord182 db "did she seek her",0 
+ord183 db "he wishes",0 
+ord184 db "these three hens",0 
+ord185 db "he sees her",0 
+ord186 db "these sheep hide",0 
+ord187 db "she sees him",0 
+ord188 db "the fish is sri",0 
+ord189 db "she used this",0 
+ord190 db "she sees ash",0 
+ord191 db "she sees her sheep",0 
+ord192 db "she sees bees",0 
+ord193 db "the sri sister",0 
+ord194 db "seems to hide",0 
+ord195 db "side of cheese",0 
+ord196 db "the size of her",0 
+ord197 db "set of sheets",0 
+ord198 db "he seeks cheese",0 
+ord199 db "side of his sister",0 
+ord200 db "this is his sheep",0 
+ord201 db "hide these sheep",0 
+ord202 db "use her sheets",0 
+ord203 db "her sister sighs",0 
+ord204 db "is he here",0 
+ord205 db "he is she",0 
+ord206 db "her 55 sheep",0 
+ord207 db "hash es fish",0 
+ord208 db "she wishes",0 
+ord209 db "these three dishes",0 
+ord210 db "bees seek his hash",0 
+ord211 db "she sees his trees",0 
+ord212 db "she has ash",0 
+ord213 db "her sister sees dishes",0 
+ord214 db "these fishes",0 
+ord215 db "sense of size",0 
+ord216 db "hide his sister",0 
+ord217 db "side of cheese",0 
+ord218 db "she sighs",0 
+ord219 db "The cheese is his",0 
+ord220 db "her sister sees bees",0 
+ord221 db "is her sister here",0 
+ord222 db "these wishes",0 
+ord223 db "Here is Tennessee",0 
+ord224 db "this is fish",0 
+ord225 db "Mississippi fish",0 
+ord226 db "size of him",0 
+ord227 db "he sighs",0 
+ord228 db "his 55 fish",0 
+ord229 db "she es he",0 
+ord230 db "kiss his sheets",0 
+ord231 db "sheer sheets",0 
+ord232 db "she is 55",0 
+ord233 db "his sri trees",0 
+ord234 db "he is sri",0 
+ord235 db "kiss the sheep",0 
+ord236 db "sheer the sheep",0 
+ord237 db "he kisses her",0 
+ord238 db "side of sheep",0 
+ord239 db "he sees her trees",0 
+ord240 db "hide the sheets",0 
+ord241 db "his sheep sigh",0 
+ord242 db "55 bees es 55 sheep",0 
+ord243 db "sheer sheets",0 
+ord244 db "she is sissier",0 
+ord245 db "he is 55",0 
+ord246 db "these bees are his",0 
+ord247 db "hide the dishes",0 
+ord248 db "three sri trees",0 
+ord249 db "she is sri",0 
+ord250 db "seems she is",0 
+ord251 db "this is him",0 
+ord252 db "55 bees",0 
+ord253 db "is she here",0 
+ord254 db "he sheers her sheep",0 
+ord255 db "she kisses her sister",0 
+ord256 db "fish in the sea",0 
+ord257 db "here is hash",0 
+ord258 db "these dishes",0 
+ord259 db "this is cheese",0 
+ord260 db "she sighs",0 
+ord261 db "he uses these trees",0 
+ord262 db "set the dishes",0 
+ord263 db "55 sisters",0 
+ord264 db "55 sheep",0 
+ord265 db "she seeks ash",0 
+ord266 db "she said shh",0 
+ord267 db "her sisters sense",0 
+ord268 db "these fish at sea",0 
+ord269 db "side of his sheep",0 
+ord270 db "his sense of her",0 
+ord271 db "sissy sissies",0 
+ord272 db "she sees cheese",0 
+ord273 db "he uses the dishes",0 
+ord274 db "hiss her sheets",0 
+ord275 db "her sense",0 
+ord276 db "side of his cheese",0 
+ord277 db "is in these trees",0 
+ord278 db "this is ash",0 
+ord279 db "her sri sister",0 
+ord280 db "she uses the dishes",0 
+ord281 db "did he seek this",0 
+ord282 db "her fish",0 
+ord283 db "did he see this",0 
+ord284 db "he hisses at her",0 
+ord285 db "bees seek them",0 
+ord286 db "he sees bees",0 
+ord287 db "this is her sheep",0 
+ord288 db "Here is Mississippi",0 
+ord289 db "her sri sister",0 
+ord290 db "did he see this",0 
+ord291 db "she wishes",0 
+ord292 db "sheer sheets",0 
+ord293 db "she sighs at him",0 
+ord294 db "the sister uses this",0 
+ord295 db "side of his cheese",0 
+ord296 db "his sheep sigh",0 
+ord297 db "side of his sister",0 
+ord298 db "she sees her sheep",0 
+ord299 db "hide the sheets",0 
+ord300 db "Tennesse bees",0 
+ord301 db "these dishes",0 
+ord302 db "55 bees es 55 sheep",0 
+ord303 db "the size of her",0 
+ord304 db "he is sri",0 
+ord305 db "did he seek this",0 
+ord306 db "set of sheets",0 
+ord307 db "hash es fish",0 
+ord308 db "sheer sheets",0 
+ord309 db "did she see this",0 
+ord310 db "seems she is",0 
+ord311 db "she kisses her sister",0 
+ord312 db "The cheese is his",0 
+ord313 db "sense of size",0 
+ord314 db "she uses the dishes",0 
+ord315 db "hens es bees",0 
+ord316 db "size of her sister",0 
+ord317 db "Here is Mississippi",0 
+ord318 db "these fish at sea",0 
+ord319 db "Here is Tennessee",0 
+; Midlertidig liste af word_ptrs
+word_ptrs dd ord1,ord2,ord3,ord4,ord5,ord6,ord7,ord8,ord9,ord10 
+ dd ord11,ord12,ord13,ord14,ord15,ord16,ord17,ord18,ord19,ord20 
+ dd ord21,ord22,ord23,ord24,ord25,ord26,ord27,ord28,ord29,ord30 
+ dd ord31,ord32,ord33,ord34,ord35,ord36,ord37,ord38,ord39,ord40 
+ dd ord41,ord42,ord43,ord44,ord45,ord46,ord47,ord48,ord49,ord50 
+ dd ord51,ord52,ord53,ord54,ord55,ord56,ord57,ord58,ord59,ord60 
+ dd ord61,ord62,ord63,ord64,ord65,ord66,ord67,ord68,ord69,ord70 
+ dd ord71,ord72,ord73,ord74,ord75,ord76,ord77,ord78,ord79,ord80 
+ dd ord81,ord82,ord83,ord84,ord85,ord86,ord87,ord88,ord89,ord90 
+ dd ord91,ord92,ord93,ord94,ord95,ord96,ord97,ord98,ord99,ord100 
+ dd ord101,ord102,ord103,ord104,ord105,ord106,ord107,ord108,ord109,ord110 
+ dd ord111,ord112,ord113,ord114,ord115,ord116,ord117,ord118,ord119,ord120 
+ dd ord121,ord122,ord123,ord124,ord125,ord126,ord127,ord128,ord129,ord130 
+ dd ord131,ord132,ord133,ord134,ord135,ord136,ord137,ord138,ord139,ord140 
+ dd ord141,ord142,ord143,ord144,ord145,ord146,ord147,ord148,ord149,ord150 
+ dd ord151,ord152,ord153,ord154,ord155,ord156,ord157,ord158,ord159,ord160 
+ dd ord161,ord162,ord163,ord164,ord165,ord166,ord167,ord168,ord169,ord170 
+ dd ord171,ord172,ord173,ord174,ord175,ord176,ord177,ord178,ord179,ord180 
+ dd ord181,ord182,ord183,ord184,ord185,ord186,ord187,ord188,ord189,ord190 
+ dd ord191,ord192,ord193,ord194,ord195,ord196,ord197,ord198,ord199,ord200 
+ dd ord201,ord202,ord203,ord204,ord205,ord206,ord207,ord208,ord209,ord210 
+ dd ord211,ord212,ord213,ord214,ord215,ord216,ord217,ord218,ord219,ord220 
+ dd ord221,ord222,ord223,ord224,ord225,ord226,ord227,ord228,ord229,ord230 
+ dd ord231,ord232,ord233,ord234,ord235,ord236,ord237,ord238,ord239,ord240 
+ dd ord241,ord242,ord243,ord244,ord245,ord246,ord247,ord248,ord249,ord250 
+ dd ord251,ord252,ord253,ord254,ord255,ord256,ord257,ord258,ord259,ord260 
+ dd ord261,ord262,ord263,ord264,ord265,ord266,ord267,ord268,ord269,ord270 
+ dd ord271,ord272,ord273,ord274,ord275,ord276,ord277,ord278,ord279,ord280 
+ dd ord281,ord282,ord283,ord284,ord285,ord286,ord287,ord288,ord289,ord290 
+ dd ord291,ord292,ord293,ord294,ord295,ord296,ord297,ord298,ord299,ord300 
+ dd ord301,ord302,ord303,ord304,ord305,ord306,ord307,ord308,ord309,ord310 
+ dd ord311,ord312,ord313,ord314,ord315,ord316,ord317,ord317,ord318,ord319
